36d63f82e6ba2c2e65325974f48243767eb1a15f
Rushabh shah <rshah19@horizon.csueastbay.edu>
2018-04-26 19:31:24 -0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+module.exports = router;*/
+var express = require('express');
+var mongodb = require('mongodb');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
+
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect('mongodb://heroku_kx9zmnld:pmn23m892ji0hm0m5tcil7edql@ds019882.mlab.com:19882/heroku_kx9zmnld', function(err, client) {
+        // mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {  // works with mongodb v2 but not v3
+        if(err) throw err;
+        //get collection of routes
+        var db = client.db('heroku_kx9zmnld');  // in v3 we need to get the db from the client
+        var Routes = db.collection('Routes');
+        //get all Routes with frequency >=1
+        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        // db.close(function (err) {
+        client.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+});//end app.get
